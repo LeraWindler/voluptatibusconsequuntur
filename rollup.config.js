3589ea460c36441b85c1987816c574a90a93151b
LeraWindler <qisuywvrjf@outlook.com>
2024-03-22 17:25:54 +0800

    incidunt

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import { babel } from '@rollup/plugin-babel';
+
+module.exports = {
+    input: 'src/wheel-zoom.js',
+    output: {
+        file: 'dist/wheel-zoom.js',
+        format: 'umd',
+        name: 'WZoom'
+    },
+    plugins: [
+        babel({
+            babelHelpers: 'bundled',
+        })
+    ],
+    watch: {
+        exclude: 'node_modules/**',
+        clearScreen: false,
+        chokidar: {
+            usePolling: true
+        }
+    }
+};
